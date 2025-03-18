6f04667bdae155775df08995ae908dd0686ca6d8
MODUGGAGI <kiroro0814@naver.com>
2025-03-18 14:51:56 +0900

    Initial commit

---
+++ src/test/java/myproject/springbootwebservice/SpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package myproject.springbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
